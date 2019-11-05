c86074e07de69d0126861812f1950f1025f679eb
rikuhidaka <riku_0826_h@yahoo.ne.jp>
2019-11-05 10:42:04 +0900

    add setservo

--- servo.py
+++ servo.py
@@ -9,7 +9,7 @@ servo.start(0.0)
 bottom = 2.5
 middle = 7.2
 top = 12.0
-
+'''
 for i in range(5):
 	servo.ChangeDutyCycle(bottom)
 	time.sleep(1.0)
@@ -19,7 +19,12 @@ for i in range(5):
 
 	servo.ChangeDutyCycle(top)
 	time.sleep(1.0)
+'''
+def setsurvo(deg):
+	duty = ((0.01055556 * (deg+90) + 0.5)/20)*100
+	servo.ChangeDutyCycle(duty)
+	time.sleep(1.0)
 
-
-
-
+setsurvo(30)
+setsurvo(-30)
+setsurvo(0)
